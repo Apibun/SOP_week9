83a19de787e7e9c28d1e5de515c4e2155493dc12
Apibun <73689795+Apibun@users.noreply.github.com>
2022-10-11 11:08:04 +0700

    Add files via upload

---
+++ product-service/product-service/src/test/java/com/sop/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
